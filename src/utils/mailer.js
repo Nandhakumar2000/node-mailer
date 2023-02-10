326a0d837b207e4eea4c0c74912875b8467f9f32
Nandhakumar V <nandhakumar@192.168.1.61>
2023-02-10 12:28:53 +0530

    feat for mail and deploy

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,8 +6,8 @@ const nodeMailer = require('nodemailer')
 
 // Những thông tin dưới đây các bạn có thể ném nó vào biến môi trường env nhé.
 // Vì để demo nên mình để các biến const ở đây.
-const adminEmail = 'Thay thế chuỗi string này thành địa chỉ admin email của bạn.'
-const adminPassword = 'Thay thế chuỗi string này thành mật khẩu admin email của bạn.'
+const adminEmail = 'nandhuv2000@gmail.com'
+const adminPassword = 'ybipwaxuxudfshog'
 // Mình sử dụng host của google - gmail
 const mailHost = 'smtp.gmail.com'
 // 587 là một cổng tiêu chuẩn và phổ biến trong giao thức SMTP
@@ -22,7 +22,10 @@ const sendMail = (to, subject, htmlContent) => {
     auth: {
       user: adminEmail,
       pass: adminPassword
-    }
+    },
+    tls: {
+  rejectUnauthorized: false
+}
   })
 
   const options = {
